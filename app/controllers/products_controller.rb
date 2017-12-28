c8c27b8c1e4cc92cd16ff8297b88e1a5566c795a
leotangram <leotangram@gmail.com>
2017-12-28 12:01:52 -0500

    Modificando ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,12 +7,6 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  # GET /products/1
-  # GET /products/1.json
-  def show
-    # @product = Product.find(params[:id])
-  end
-
   # GET /products/new
   def new
     @product = Product.new
@@ -21,36 +15,29 @@ class ProductsController < ApplicationController
 
   # GET /products/1/edit
   def edit
-    @categories = Category.all
   end
 
   # POST /products
-  # POST /products.json
+
   def create
     @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.save
+      flash[:notice] = 'The Category has been saved'
+      redirect_to products_path
+    else
+      render :new
     end
+
   end
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.update_attributes(product_params)
+      flash[:notice] = "Store was successfully updated."
+      redirect_to products_path
+    else
+      render :edit
     end
   end
 
@@ -58,10 +45,8 @@ class ProductsController < ApplicationController
   # DELETE /products/1.json
   def destroy
     @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to products_path
+
   end
 
   private
@@ -74,4 +59,3 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name, :price, :category_ids => [])
     end
-end
